bf8340d92d0166e437a5381344429c99593712f4
XhstormR <10527522+XhstormR@users.noreply.github.com>
2020-01-15 16:11:53 +0800

    Signed-off-by: XhstormR <10527522+XhstormR@users.noreply.github.com>

--- assets/run.bat
+++ assets/run.bat
@@ -1,1 +1,4 @@
 curl -LOk https://maven.aliyun.com/repository/jcenter/com/github/spotbugs/spotbugs/4.0.0-beta4/spotbugs-4.0.0-beta4.zip
+
+:: https://github.com/mebigfatguy/fb-contrib
+:: https://github.com/find-sec-bugs/find-sec-bugs
